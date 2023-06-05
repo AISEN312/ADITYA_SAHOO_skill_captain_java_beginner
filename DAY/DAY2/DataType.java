a85f41116ef20e06735459de925d28848e2372bb
aditya sahoo <111141966+AISEN312@users.noreply.github.com>
2023-06-05 20:51:27 +0530

    fc

---
+++ DAY/DAY2/DataType.java
@@ -0,0 +1,25 @@
+package com.helloworld;
+
+public class DataType {
+    public static void main(String[]args){
+        byte wByte = 5;
+        short wShort = 50;
+        int wInt = 500;
+        long wLong = 20000L;
+        float wFloat = 3.14f;
+        double wDouble = 3.1415;
+        char wChar = 'B';
+        boolean wBoolean = false;
+        string wString = "hello";
+        System.out.println("Byte value: " + wByte);
+        System.out.println("Short value: " + wshort);
+        System.out.println("Int value: " + wint);
+        System.out.println("Long value: " + wlong);
+        System.out.println("Float value: " + wfloat);
+        System.out.println("Double value: " + wdouble);
+        System.out.println("Character value: " + wchar);
+        System.out.println("Boolean value: " + wboolean);
+        System.out.println("String value: " + wstring);
+
+    }
+}
